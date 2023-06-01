7d23a971296f2e1ca8800a1359f5e5cfa18d6260
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2023-06-01 14:34:48 +0300

    init project

--- archives/views.py
+++ archives/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+
+from archives.models import Category
+
 
 # Create your views here.
+
+class CategoriesView(View):
+    template_name = 'categories.html'
+    model = Category
+    context_object_name = 'categories'
+
