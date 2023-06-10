0edba0a0e1e2ea700ff7a03e0cf4180cca08aba3
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2023-06-10 09:44:35 +0300

    KAAREL - admin views generation

--- archives/admin.py
+++ archives/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-
 from archives.models import User, Role, Category, AgeRating, Product
 
 admin.site.register(User)
@@ -8,12 +7,3 @@ admin.site.register(Role)
 admin.site.register(Category)
 admin.site.register(AgeRating)
 admin.site.register(Product)
-
-
-
-
-
-
-
-
-
