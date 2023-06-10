439432f184aebcf9eff31929d4f575a7fe56c2da
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2023-06-10 09:05:41 +0300

    KAAREL - admin views generation

--- archives/admin.py
+++ archives/admin.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
 
 class AuthorAdmin(admin.ModelAdmin):
     pass
 
 
-class Author:
-    pass
 
 
-admin.site.register(Author, AuthorAdmin)
+
