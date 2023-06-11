26c87d48e3df3b6b556655540c6cdde98b5ada86
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2023-06-11 09:10:15 +0300

    KAAREL - admin views generation

--- archives/views.py
+++ archives/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from utils import forms
+
 
 from archives.models import Category, Product
 
@@ -39,9 +41,27 @@ def login_view(request):
         status = request.POST.get('status')
         user = authenticate(request, email=username, password=password, status=status)
         if user is not None and status == 'A':
-            login_view(request, user)
-            return redirect(reverse('store:store'))
+            login(request, user)
+            return redirect(reverse('store/store.html'))
         else:
             return JsonResponse({'error': 'Invalid credentials'})
     else:
         return JsonResponse({'error': 'Invalid request method'})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('index.html'))
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = forms.RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('index.html'))
+
+    else:
+        form = forms.RegisterUserForm()
+    return render(request, './user/register.html', {'form': form})
+
